097fd5331b94355f21cb95db28caffaa032101b0
Julien CARACO <j.caraco@game-sup.fr>
2025-11-26 10:02:52 +0100

    Debut d'implementation de la life du Nexus

--- Unity/Workshop TD/Assets/Scripts/Enemis/Enemy_Move.cs
+++ Unity/Workshop TD/Assets/Scripts/Enemis/Enemy_Move.cs
@@ -8,10 +8,13 @@ public class Enemy_Move : MonoBehaviour
     [SerializeField] private NavMeshAgent agent;
     [SerializeField] private GameObject target;
 
+    [SerializeField] public Life_Manageur Life_Manageur;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        target = GameObject.Find("Enemy Target");
+        target = GameObject.Find("Enemy Target / Nexus");
+        Life_Manageur = target.GetComponent<Life_Manageur>();
     }
 
     // Update is called once per frame
@@ -22,8 +25,8 @@ public class Enemy_Move : MonoBehaviour
         if (Vector3.Distance(transform.position, target.transform.position) < 0.2f)
         {
             Destroy(gameObject);
+            Life_Manageur.Life_Nexus--;
             return;
         }
-
     }
 }
