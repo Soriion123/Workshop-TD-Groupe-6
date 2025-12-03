4a55466ef9fb8991593084ce633555748085de35
Julien CARACO <j.caraco@game-sup.fr>
2025-12-03 11:37:00 +0100

    Start Ui manageur + Test LD 4

---
+++ Unity/Workshop TD/Assets/Scripts/UI_Manageur.cs
@@ -0,0 +1,27 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class UI_Manageur : MonoBehaviour
+{
+
+    // Les scriptes qui demande une UI
+    public GameManager gameManager;
+    public Nexus Nexus;
+    public Cliqueur cliqueur;
+
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
