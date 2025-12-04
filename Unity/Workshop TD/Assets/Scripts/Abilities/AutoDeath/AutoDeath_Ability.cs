237ede82a76772200f202ef760598cedd0612a86
gabin <garnaudbadoy@gmail.com>
2025-12-04 14:31:02 +0100

    autodeath V1

---
+++ Unity/Workshop TD/Assets/Scripts/Abilities/AutoDeath/AutoDeath_Ability.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoDeath_Ability : MonoBehaviour
+{
+    public GameObject AutoDeath_Zone;
+    public KeyCode activationKey = KeyCode.R;
+
+    private bool isReady = true;
+    private Mecha_Inventory mecha_Inventory;
+
+    private void Start()
+    {
+        mecha_Inventory = GetComponent<Mecha_Inventory>();
+
+        if (AutoDeath_Zone != null)
+            AutoDeath_Zone.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(activationKey)
+            && isReady
+            && mecha_Inventory.HasAutoDeathToken())
+        {
+            StartCoroutine(ActivateAndDie());
+        }
+    }
+
+    private IEnumerator ActivateAndDie()
+    {
+        isReady = false;
+
+        // Consomme la clé
+        mecha_Inventory.ConsumeAutoDeathToken();
+
+        // Active la zone
+        AutoDeath_Zone.SetActive(true);
+
+        // Attente de 1 seconde
+        yield return new WaitForSeconds(1f);
+
+        // Destruction du mecha
+        Destroy(gameObject);
+    }
+}
